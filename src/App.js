71910d435638845b3205bb278a4407073bf56528
osta1647 <72413097+osta1647@users.noreply.github.com>
2021-01-09 14:54:11 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import WeatherSearch from "./WeatherSearch";
+import "./styles.css";
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <WeatherSearch />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
+
+export default App;
